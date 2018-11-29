690bb9036310ade97e1178033f8d364c63d0cb2c
DirtySpawn <seancunningham1216@gmail.com>
2018-11-29 14:20:17 -0500

    first commit

--- virtuaplant-master/plants/powerplant/powerplantworld.py
+++ virtuaplant-master/plants/powerplant/powerplantworld.py
@@ -78,7 +78,7 @@ PLC_CONDENSER_WATER_LEVEL_LOW = 0X06
 PLC_CONDENSER_WATER_LEVEL_MIN = 0X07
 PLC_CONDENSER_WATER_LEVEL_MAX = 0X08
 PLC_CONDENSER_WATER_VALVE = 0X09
-PLC_VOLUME = 0x10
+PLC_BOILER_WATER_VOLUME = 0x10
 
 # Collision Types
 
@@ -109,8 +109,8 @@ def add_water(space):
     body = pymunk.Body(mass, inertia)
     body._bodycontents.v_limit = 120
     body._bodycontents.h_limit = 1
-    x = random.randint(169, 170)
-    body.position = x, 348
+    #x = random.randint(169, 170)
+    #body.position = x, 348
     shape = pymunk.Circle(body, radius, (0, 0))
     shape.friction = 0.0
     shape.collision_type = ball_collision #liquid
@@ -118,8 +118,8 @@ def add_water(space):
     return shape
 
 def add_steam(space):
-    mass = 0.1
-    radius = 4
+    mass = 0.8
+    radius = 2
     inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
     body = pymunk.Body(mass, inertia)
     body._bodycontents.v_limit = 120
@@ -170,10 +170,27 @@ def add_condenser_outlet_valve(space):
 def add_watermain(space):
 
     body = pymunk.Body()
-    body.position = (170, 350)
+    body.position = (100, 360)
     shape = pymunk.Poly.create_box(body, (15, 20), (0, 0), 0)
     space.add(shape)
     return shape
+
+def add_light1(space):
+
+    body = pymunk.Body()
+    body.position = (480, 494)
+    #y -45, 45
+    #x 0, 120    
+    l1 = pymunk.Segment( body, (0,0),(10,25), 3 )
+    l2 = pymunk.Segment( body, (10,25), (20, 0), 3)
+    l3 = pymunk.Segment( body, (20, 0), ( 55, 40), 3 )
+    l4 = pymunk.Segment( body, (55, 40), (60, -30), 3 )
+    l5 = pymunk.Segment( body, (60, -30), (100, -45), 3 )
+    l6 = pymunk.Segment( body, (100, -45), (120, 0), 3 )
+
+    space.add( l1, l2, l3, l4, l5, l6 )
+
+    return ( l1, l2, l3, l4, l5, l6  )
 
 def add_boiler(space):
 	body = pymunk.Body()
@@ -198,21 +215,37 @@ def add_condenser(space):
 
 	#Condenser Lines
 
-	l1 = pymunk.Segment( body, (0,0), (220,35), 3 )
-	l2 = pymunk.Segment( body, (220,15), (220, 200), 3)
-	l3 = pymunk.Segment( body, (220, 200), (100, 200), 3 )
-	l4 = pymunk.Segment( body, (0, 25), (100, 35), 3 )
-	l5 = pymunk.Segment( body, (100, 55), (100, 65), 3)
-	l6 = pymunk.Segment( body, (100, 55), (100, 200), 3)
-	l7 = pymunk.Segment( body, (100, 35), ( 35, 45), 3)
-	l8 = pymunk.Segment( body, (100, 55), (65, 55), 3)
-	l9 = pymunk.Segment( body, (35,45), (35, 205), 3 )
-	l10 = pymunk.Segment( body, (65, 55), (65, 205), 3)
+	l1 = pymunk.Segment( body, (0,0), (65, 35), 3 ) # (220,25), 3 ) #bottom line
+	#l2 = pymunk.Segment( body, (220,15), (220, 200), 3) # right line
+	#l3 = pymunk.Segment( body, (220, 200), (100, 200), 3 ) #top line
+
+	l4 = pymunk.Segment( body, (0, 25), (35, 50), 3 )  # (100, 35), 3 ) 
+
+	#l5 = pymunk.Segment( body, (100, 55), (100, 65), 3)
+	#l6 = pymunk.Segment( body, (100, 55), (100, 200), 3)
+	#l7 = pymunk.Segment( body, (100, 35), ( 35, 45), 3)
+	#l8 = pymunk.Segment( body, (100, 55), (65, 55), 3)
+	l9 = pymunk.Segment( body, (35,50), (35, 205), 3 )
+	l10 = pymunk.Segment( body, (65, 35), (65, 205), 3)
 
 
-	space.add(l1, l2, l3, l4, l5, l6, l7, l8, l9, l10)
+	space.add(l1, l4, l9, l10)
 
-	return (l1, l2, l3, l4, l5, l6, l7, l8, l9, l10)
+	return (l1, l4, l9, l10)
+
+def add_turbine(space):
+    body = pymunk.Body()
+    body.position = (32, 465)
+    #Boiler
+    l1 = pymunk.Segment(body, (0, 0), (0, 70), 3)
+    l2 = pymunk.Segment(body, (20, 0), (30, 10), 3)
+    l3 = pymunk.Segment(body, (0, 70), (225, 80), 3)
+    l5 = pymunk.Segment(body, (30, 10), (188, 0), 3)
+    l6 = pymunk.Segment(body, (225, 80), (215, 0), 3)
+
+    space.add(l1, l2, l3, l5, l6)
+
+    return (l1, l2, l3, l5, l6)
 
 # Draw a defined polygon
 def draw_polygon(bg, shape):
@@ -235,14 +268,17 @@ def draw_line(screen, line, color = None):
         pygame.draw.lines(screen, color, False, [p1,p2])
     
 # Draw lines from an iterable list
-def draw_lines(screen, lines):
+def draw_lines(screen, lines, color = None):
     for line in lines:
         body = line.body
         pv1 = body.position + line.a.rotated(body.angle) # 1
         pv2 = body.position + line.b.rotated(body.angle)
         p1 = to_pygame(pv1) # 2
         p2 = to_pygame(pv2)
-        pygame.draw.lines(screen, THECOLORS["black"], False, [p1,p2])
+        if color is None:
+            pygame.draw.lines(screen, THECOLORS["black"], False, [p1,p2])
+        else:
+            pygame.draw.lines(screen, color, False, [p1,p2])
 
 # Default collision function for objects
 # Returning true makes the two objects collide normally just like "walls/pipes"
@@ -291,22 +327,29 @@ def run_world():
     condenser = add_condenser(space)
     condenser_valve = add_condenser_outlet_valve(space)
     watermain = add_watermain(space)
+    turbine = add_turbine(space)
+    turbair = add_turbine(air)
 
+    light1 = add_light1(space)
 
     waters = []
     WATERRATE = 1
     ticks_to_next_water = WATERRATE
 
-    STEAMRATE = 500
+    STEAMRATE = 1600
+    STEAMMAXDIST = 600
     ticks_to_convert_steam = STEAMRATE
 
     fires = []
-    FIRERATE = 1
+    FIRERATE = 2
     ticks_to_next_fire = FIRERATE
 
     steams = []
-    NEXTSTEAMRATE = 6
+    NEXTSTEAMRATE = 2
     ticks_to_next_steam = NEXTSTEAMRATE
+
+    CONVERTFROMSTEAM = 10
+    ticks_to_convert_to_water = CONVERTFROMSTEAM
 
     # Set font settings
     fontBig = pygame.font.SysFont(None, 40)
@@ -344,13 +387,13 @@ def run_world():
 
         if PLCGetTag(PLC_CONDENSER) == 1:
             space.add_collision_handler( condenser_outlet_valve_collision, ball_collision, begin=condenser_valve_open )
-            if( shift):
+            if(shift):
                 shift = False
                 space.gravity = (500.0, 0.0)
 
         elif PLCGetTag(PLC_CONDENSER) == 0:
             space.add_collision_handler( condenser_outlet_valve_collision, ball_collision, begin=condenser_valve_closed )
-            pass
+            shift = True
         elif ( PLCGetTag(PLC_CONDENSER) > 1 and PLCGetTag(PLC_CONDENSER) < 6 ):
             pass
             #if( sensor_tick == 0 ):
@@ -392,6 +435,9 @@ def run_world():
             if ticks_to_next_water <= 0 : #and PLCGetTag(PLC_FEED_PUMP) == 1:
                 ticks_to_next_water = WATERRATE
                 water_shape = add_water(space)
+                water_shape.body.position = watermain.body.position
+                water_shape.body.position.y -= 12
+                water_shape.body.position.x = random.randint( watermain.body.position.x - 1, watermain.body.position.x + 1 )
                 waters.append(water_shape)
             
         water_to_remove = []
@@ -400,36 +446,48 @@ def run_world():
 
         for water in waters:
             draw_ball(bg, water, 'blue')
-            if water.body.position.x < 180:
+            if water.body.position.x < 180 and water.body.position.y < 300: # for water in boiler
                 wateramount += 1
-            if ( (PLCGetTag(PLC_CONDENSER_WATER_LEVEL_LOW) == 1) or (PLCGetTag(PLC_CONDENSER_WATER_LEVEL_MIN) == 1) or (PLCGetTag(PLC_CONDENSER_WATER_LEVEL_MAX) == 1) ) and (PLCGetTag( PLC_FUEL ) == 1 ):
+            if (PLCGetTag(PLC_FUEL) == 1):
                 if ticks_to_convert_steam <= 0:
-                    if( water.body.position.x < 180 ):
+                    if( water.body.position.x < 180 and water.body.position.y < 300 ):
                         water_to_remove.append(water)
                         ticks_to_convert_steam = STEAMRATE
 
-                        ticks_to_next_steam -=1
+                        ticks_to_next_steam -= 1
                         if ticks_to_next_steam <= 0 :
                             ticks_to_next_steam = NEXTSTEAMRATE
                             steam_shape = add_steam(air)
                             steams.append( steam_shape )
 
                 else:
-                    ticks_to_convert_steam -= 1    
+                    ticks_to_convert_steam -= 1
+
+        steam_to_remove = []
+        for steam in steams:
+            if steam.body.position.x > 225 and steam.body.position.y > 400:
+                if ticks_to_convert_to_water <= 0:
+                    steam_to_remove.append(steam)
+                    ticks_to_convert_to_water = CONVERTFROMSTEAM
+                else:
+                    ticks_to_convert_to_water -= 1
+
+        for steam in steam_to_remove:
+            pos = steam.body.position
+            watershape = add_water(space)
+            watershape.body.position = pos
+            waters.append( watershape)
+            air.remove(steam, steam.body)
+            steams.remove(steam)
 
         if wateramount  <= 0:
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_LOW,0 )
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_MIN,0 )
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_MAX,0 )
-
-            PLCSetTag(PLC_FUEL, 0)
-            PLCSetTag(PLC_CONDENSER_WATER_VALVE, 1)
         elif( (wateramount >= LOWAMOUNT) and ( wateramount < MINAMOUNT) ):
             PLCSetTag( PLC_CONDENSER_WATER_LEVEL_LOW, 1)
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_MIN,0 )
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_MAX,0 )
-
-            PLCSetTag(PLC_CONDENSER_WATER_VALVE, 1 )
         elif( (wateramount >= MINAMOUNT) and ( wateramount < MAXAMOUNT) ):
             PLCSetTag( PLC_CONDENSER_WATER_LEVEL_MIN, 1)
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_LOW,0 )
@@ -439,7 +497,6 @@ def run_world():
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_MIN,0 )
             PLCSetTag(PLC_CONDENSER_WATER_LEVEL_LOW,0 )
 
-            PLCSetTag(PLC_CONDENSER_WATER_VALVE,0)
 
         for water in water_to_remove:
         	space.remove(water, water.body)
@@ -456,7 +513,7 @@ def run_world():
         steam_to_remove = []
 
         for steam in steams:
-            if steam.body.position.y > 500:
+            if steam.body.position.y > STEAMMAXDIST:
                 steam_to_remove.append(steam)
 
             draw_ball( bg, steam, 'gray')
@@ -470,7 +527,11 @@ def run_world():
         draw_lines(bg, condenser)
         draw_line(bg, condenser_valve, THECOLORS['red'])
         draw_polygon(bg, watermain)
- 
+        draw_lines(bg, turbine)
+
+        draw_lines(bg, light1) # , THECOLORS['red'])
+
+
         text = str(wateramount)
 
         textsurface = myfont.render( text, False, (0,0,0))
