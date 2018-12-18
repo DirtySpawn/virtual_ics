28d88d3def46afe560f37d932eb247fabc760957
borktrain <tristan.bourque22@gmail.com>
2018-12-18 09:17:31 -0500

    changes to generator and pylon

--- virtuaplant-master/plants/powerplant/powerplantplcpylon.py
+++ virtuaplant-master/plants/powerplant/powerplantplcpylon.py
@@ -18,13 +18,13 @@ class MyParser(argparse.ArgumentParser):
         sys.stderr.write('error: %s\n' % message)
         self.print_help()
         sys.exit(2)
-        
+
 # Create argparser object to add command line args and help option
 parser = MyParser(
 	description = 'This Python script runs the SCADA HMI to control the PLC',
 	epilog = '',
 	add_help = True)
-	
+
 # Add a "-i" argument to receive a filename
 parser.add_argument("-t", action = "store", dest="server_addr",
 					help = "Modbus server IP address to connect the HMI to")
@@ -41,7 +41,7 @@ MODBUS_SLEEP=1
 PLC_PYLON = 0x0d
 
 class HMIWindow(Gtk.Window):
-    
+
     def initModbus(self):
         # Create modbus connection to specified address and port
         self.modbusClient = ModbusClient(args.server_addr, port=5020)
@@ -50,13 +50,13 @@ class HMIWindow(Gtk.Window):
     def resetLabels(self):
         self.pylon_plc_online_value.set_markup("<span weight='bold' foreground='red'>OFF</span>")
         self.pylon_plc_operational_value .set_markup("<span weight='bold' foreground='black'>N/A</span>")
-        
-        
+
+
     def __init__(self):
         # Window title
         Gtk.Window.__init__(self, title="PYLON PLC")
         self.set_border_width(100)
-        
+
         #Create modbus connection
         self.initModbus()
 
@@ -69,36 +69,50 @@ class HMIWindow(Gtk.Window):
 
         # Main title label
         label = Gtk.Label()
-        label.set_markup("<span weight='bold' size='xx-large' color='black'>PLC : PYLON</span>")
+        label.set_markup("<span weight='bold' size='xx-large' color='black'>PLC : PYLON/POWER GAUGE</span>")
         grid.attach(label, 4, elementIndex, 4, 1)
         elementIndex += 1
 
-        # Crude Oil Feed Pump
+        # PYLON
+        # Connected to World / HMI
         pylon_plc_online_label = Gtk.Label("Online: ")
         pylon_plc_online_value = Gtk.Label()
 
-        pylon_plc_operational_label = Gtk.Label("Operational: ")
-        pylon_plc_operational_value = Gtk.Label()
-        pylon_plc_operational_on_button = Gtk.Button("ON")
-        pylon_plc_operational_off_button = Gtk.Button("OFF")   
+        # Valve / Button to Turn ON/OFF
+        pylon_plc_status_label = Gtk.Label("Pylon Status: ")
+        pylon_plc_status_value = Gtk.Label()
+        pylon_plc_status_on_button = Gtk.Button("ON")
+        pylon_plc_status_off_button = Gtk.Button("OFF")
+        pylon_plc_status_on_button.connect("clicked", self.setpylonStatus, 1)
+        pylon_plc_status_off_button.connect("clicked", self.setpylonStatus, 0)
 
-        pylon_plc_operational_on_button.connect("clicked", self.setPylonOperational, 1)
-        pylon_plc_operational_off_button.connect("clicked", self.setPylonOperational, 0)
+        #pylon power gauge
+        pylon_plc_power_label = Gtk.Label("Power Level: ")
+        pylon_plc_power_value = Gtk.Label()
 
+        # pylon
+        # Connected to World / HMI *****PART 2
         grid.attach(pylon_plc_online_label, 4, elementIndex, 1, 1)
         grid.attach(pylon_plc_online_value, 5, elementIndex, 1, 1)
         elementIndex += 1
-        
-        grid.attach(pylon_plc_operational_label, 4, elementIndex, 1, 1)
-        grid.attach(pylon_plc_operational_value, 5, elementIndex, 1, 1)
-        grid.attach(pylon_plc_operational_on_button, 6, elementIndex, 1, 1)
-        grid.attach(pylon_plc_operational_off_button, 7, elementIndex, 1, 1)
+
+        # Valve / Button to Turn ON/OFF *****PART 2
+        grid.attach(pylon_plc_status_label, 4, elementIndex, 1, 1)
+        grid.attach(pylon_plc_status_value, 5, elementIndex, 1, 1)
+        grid.attach(pylon_plc_status_on_button, 6, elementIndex, 1, 1)
+        grid.attach(pylon_plc_status_off_button, 7, elementIndex, 1, 1)
         elementIndex += 1
 
-        
+        grid.attach(pylon_plc_power_label, 4, elementIndex, 1, 1)
+        grid.attach(pylon_plc_power_value, 5, elementIndex, 1, 1)
+        elementIndex += 1
+
+		## attach  # column number
         # Attach Value Labels
         self.pylon_plc_online_value = pylon_plc_online_value
-        self.pylon_plc_operational_value = pylon_plc_operational_value
+        self.pylon_plc_status_value = pylon_plc_status_value
+        self.pylon_plc_output_value = pylon_plc_output_value
+
 
         # Set default label values
         self.resetLabels()
@@ -115,21 +129,41 @@ class HMIWindow(Gtk.Window):
 
         try:
             # Store the registers of the PLC in "rr"
-            rr = self.modbusClient.read_holding_registers(1,16)
+            rr = self.modbusClient.read_holding_registers(1,24)
             regs = []
 
             # If we get back a blank response, something happened connecting to the PLC
             if not rr or not rr.registers:
                 raise ConnectionException
-            
+
             # Regs is an iterable list of register key:values
             regs = rr.registers
 
             if not regs or len(regs) < 16:
                 raise ConnectionException
-            
+
             self.pylon_plc_online_value.set_markup("<span weight='bold' foreground='green'>ON</span>")
-            
+
+        # Change to match
+# exmaple follows
+
+            if regs[ PLC_GENERATOR_STATUS - 1 ] == 0:
+                if regs[PLC_PYLON_STATUS - 1] == 0:
+                    self.pylon_plc_status_value.set_markup("<span weight='bold' foreground='red'>OFF</span>")
+                    self.pylon_plc_power_value.set_markup("<span weight='bold' foreground='red'>No Power</span>")
+            if regs[ PLC_GENERATOR_STATUS - 1 ] == 1:
+                if regs[ PLC_PYLON_STATUS - 1 ] == 1:
+                    self.pylon_plc_status_value.set_markup("<span weight='bold' foreground='green'>ON</span>")
+                    if regs[PLC_TURBINE_RPMs - 1] == 0:
+                        self.pylon_plc_power_value.set_markup("<span weight='bold' foreground='red'>No Output</span>")
+                    if regs[PLC_TURBINE_RPMs - 1] == 1:
+                        self.pylon_plc_output_value.set_markup("<span weight='bold' foreground='gold'>Low Power</span>")
+                    if regs[PLC_TURBINE_RPMs - 1] == 2:
+                        self.pylon_plc_output_value.set_markup("<span weight='bold' foreground='green'>Normal Power</span>")
+                    if regs[PLC_TURBINE_RPMs - 1] == 3:
+                        self.pylon_plc_output_value.set_markup("<span weight='bold' foreground='crimson'>DANGER</span>")
+                        
+
             # If the feed pump "0x01" is set to 1, then the pump is running
             '''
             if regs[0] == 1:
@@ -141,7 +175,7 @@ class HMIWindow(Gtk.Window):
                 self.pylon_plc_operational_value .set_markup("<span weight='bold' foreground='red'>OFF</span>")
             elif regs[12] == 1:
                 self.pylon_plc_operational_value.set_markup("<span weight='bold' foreground='green'>ON</span>")
-            
+
 
         except ConnectionException:
             if not self.modbusClient.connect():
